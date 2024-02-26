abaa3a7c6d2c9026e7a24a8427c93e7db62ab292
Ahlam135 <a.ahlam135@gmail.com>
2024-02-27 00:14:15 +0300

    about cart and makeup page

--- frontend/src/components/pages/Makeup.jsx
+++ frontend/src/components/pages/Makeup.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
 import Header from "../Header";
 import "../../assets/styles/makeup.css";
 import { useEffect } from "react";
@@ -33,17 +32,23 @@ export default function Makeup() {
               <div className="makeup-content">
                 <p className="makeup-name">{service.servicename}</p>
                 <p className="makeup-desc">{service.servicedesc}</p>
-                <p className="makeup-duration">{service.serviceduration}hrs</p>
+                <p className="makeup-duration">
+                  Duration:{service.serviceduration}hrs
+                </p>
                 <div className="price1">
-                  <span className="home-price">{service.serviceprice}Birr</span>
-                  <a href="" className="home">
-                    Home
-                  </a>
+                  <span className="home-price">
+                    Salon Price:{service.serviceprice} Birr
+                  </span>
                 </div>
                 <div className="price2">
                   <span className="salon-price">
-                    {service.servicehomeprice} Birr
+                    Home Price:{service.servicehomeprice} Birr
                   </span>
+                </div>
+                <div className="link-cont">
+                  <a href="" className="home">
+                    Home
+                  </a>
                   <a href="" className="salon">
                     Salon
                   </a>
