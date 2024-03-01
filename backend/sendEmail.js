8cebe032422ad8dc72f636e10993a88e30a340b0
alhamdu-e <alhamdu7624@gmail.com>
2024-03-01 10:23:36 -0800

    implmenting generating random password

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,10 +1,11 @@
-let ElasticEmail = require("@elasticemail/elasticemail-client");
+require("dotenv").config();
 
+let ElasticEmail = require("@elasticemail/elasticemail-client");
+const key = process.env.API_KEY;
 let defaultClient = ElasticEmail.ApiClient.instance;
 
 let apikey = defaultClient.authentications["apikey"];
-apikey.apiKey =
-	"F49B55618A270B9B71F88D3D7E7E2E9D695F10538073F1C8610122AE2A0D56FAB23669554B8E895B0988B6048C90C92F";
+apikey.apiKey = key;
 
 const sendEmail = (recipentEmail, callback, content) => {
 	let api = new ElasticEmail.EmailsApi();
